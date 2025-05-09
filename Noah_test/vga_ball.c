d232491b059364ef9656650e3f428f19e9ec7cc3
Noah Hartzfeld <nhartzfeld@wesleyan.edu>
2025-05-09 18:03:07 -0400

    extra testing

--- Noah_test/vga_ball.c
+++ Noah_test/vga_ball.c
@@ -164,7 +164,7 @@ static int __init vga_ball_probe(struct platform_device *pdev)
 {
     // Initial values
     background_color background = { 0x00, 0x00, 0x20 }; // Dark blue
-    spaceship ship = { 0 };
+    spaceship ship = { .pos_x = 300, .pos_y = 400};
     bullet bullets[MAX_BULLETS] = { 0 };    // All bullets initially inactive
     enemy enemies[ENEMY_COUNT] = { 0 };     // All enemies initially inactive
 

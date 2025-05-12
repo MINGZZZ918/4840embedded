5711332b4c76fafe290204311c339a8219a0320e
Noah Hartzfeld <nhartzfeld@wesleyan.edu>
2025-05-11 20:37:16 -0400

    enemy movement

--- Noah_test/tim/hello.c
+++ Noah_test/tim/hello.c
@@ -292,8 +292,7 @@ void move_enemy(enemy *enemy){
 
         if (enemy->sprite == 2){
 
-
-            if(++enemy->move_time < 250)
+            if(++enemy->move_time < 500)
                 calculate_velo(ship->pos_x, ship->pos_y, enemy, 2);
             else
                 enemy->velo_x = 0;

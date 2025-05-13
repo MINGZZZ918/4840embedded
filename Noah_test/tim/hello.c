96bbea504b0a60171bc32b8eb3eda83369322bb5
Noah Hartzfeld <nhartzfeld@wesleyan.edu>
2025-05-13 01:58:30 -0400

    enemy movement

--- Noah_test/tim/hello.c
+++ Noah_test/tim/hello.c
@@ -937,7 +937,7 @@ int main(){
 
         for(int j=0; j<ENEMY_COUNT; j++){
 
-            if (game_state.enemies[j].col = 9) {
+            if (game_state.enemies[j].col == 9) {
 
                 game_state.enemies[j].active = 1;
                 printf("row: %d \n", count++);
